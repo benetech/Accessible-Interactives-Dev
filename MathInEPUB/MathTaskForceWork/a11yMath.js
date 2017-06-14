c839d7f393a9533530ea5a330e09ae96905c0745
Neil Soiffer <soiffer@alum.mit.edu>
2017-06-14 15:00:25 -0700

    fix bug in JS where aria-hidden is not unset on parent span of math element

--- MathInEPUB/MathTaskForceWork/a11yMath.js
+++ MathInEPUB/MathTaskForceWork/a11yMath.js
@@ -6,9 +6,19 @@
 // Regardless of the technique above, we still have to check if MathJax is in the page because the math
 //   won't be ready to be rendered by MathJax on page load.
 // This still isn't fool proof because other JS such as KaTeX might be doing the rendering
-function IsMathMLSupported() {
-	// call either HasFeatureMathML() or DisplaysMathML()
-	return HasFeatureMathML() || (typeof MathJax != 'undefined');
+function CanUseMathML() {
+	// This is a hueristic:
+	// There are probably cases where this should return false because the screen readers we
+	//   know about (NVDA, JAWS, VoiceOver, TalkBalk, ChomeVox) all handle MathML.
+	// So the basic assumption is that MathML is accessible if JS runs.
+	// Cases where this isn't true:
+	//		Linux (none of the above screen readers work there)
+	//		?? Non Safari on MacOS
+	var isLinux = function(){
+		var matches = navigator.userAgent.match(/Linux/);
+		return (matches!=null && matches.length==1);
+	};
+	return !isLinux();
 }
 
 // Not sure how robust the "hasFeatureMathML() implementation is, so both methods are given here
@@ -16,17 +26,26 @@ function IsMathMLSupported() {
 //  Feature Detect for MathML as w3c specification
 //  <returns>boolean: true if mathML is supported in browser
 function HasFeatureMathML(){
-	var IsChrome = function(){
-		var regex = /Chrome\/[0-9]{1,2}\.[0-9]/
-		var matches = navigator.userAgent.match(regex)
-		return (matches!=null && matches.length==1)
+	var isChrome = function(){
+		var regex = /Chrome\/[0-9]{1,2}\.[0-9]/;
+		var matches = navigator.userAgent.match(regex);
+		return (matches!=null && matches.length==1);
 	};
 	
-	var MATHML_FEATURE = "org.w3c.dom.mathml"  //as per w3c specification
-	var MATHML_FEATURE_VERSION = "2.0"         //Any version number appears to work
-	if( IsChrome() )
-		return false        					       // Not natively supported in Chrome despite what hasFeature() says 
-	return document.implementation.hasFeature(MATHML_FEATURE, MATHML_FEATURE_VERSION )
+	var isChromeOS = function(){
+		var matches = navigator.userAgent.match(/CrOS /);
+		return (matches!=null && matches.length==1);
+	};
+	
+
+	var MATHML_FEATURE = "org.w3c.dom.mathml"; // as per w3c specification
+	var MATHML_FEATURE_VERSION = "2.0";	       // any version number appears to work
+	if (isChromeOS())									 // ChromeOS doesn't support MathML, but ChromeVox and TextHELP do		
+		return true;
+
+	if (isChrome())
+		return false;       					       // Not natively supported in Chrome despite what hasFeature() says 
+	return document.implementation.hasFeature(MATHML_FEATURE, MATHML_FEATURE_VERSION );
 }
 
 function DisplaysMathML() {
@@ -49,7 +68,7 @@ function ForEach(nodeList, callback, scope) {
 
 
 function MakeMathAccessible() {
-	if (!IsMathMLSupported())
+	if (!CanUseMathML())
 		return;
 		
 	var setARIAHidden = function(element) {
@@ -57,6 +76,9 @@ function MakeMathAccessible() {
 	};
 	var unsetARIAHidden = function(element) {
 		element.setAttribute("aria-hidden", "false");
+		if (element.parentNode.tagName == "span") {
+			element.parentNode.setAttribute("aria-hidden", "false");
+		}
 	};
 	var changeImage = function(element) {
 		if (element.getAttribute("role")=="math") {
@@ -76,6 +98,8 @@ function MakeMathAccessible() {
 	
 	ForEach( document.getElementsByTagName("math"), unsetARIAHidden );
 	ForEach( document.getElementsByTagName("img"), changeImage );
+	
+	// used for HTML math case to remove the text from AT to avoid double speak
 	ForEach( document.getElementsByTagName("span"), changeMathSpan );
 	
 	// make sure an MathJax CSS math is hidden, not needed for properly done pages
